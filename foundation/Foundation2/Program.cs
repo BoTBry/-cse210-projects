8a634f25f850aba5c6cd57552ce31e088357a8c9
BoTBry <abchigbu@gmail.com>
2024-10-12 14:53:50 -0700

    f2 complete

--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -2,6 +2,122 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+public class Address
+{
+		private string street;
+		private string city;
+		private string state;
+		private string country;
+
+		public Address(string street, string city, string state, string country)
+		{
+				this.street = street;
+				this.city = city;
+				this.state = state;
+				this.country = country;
+		}
+
+		public bool IsInUSA()
+		{
+				return country.ToLower() == "usa";
+		}
+
+		public override string ToString()
+		{
+				return $"{street}, {city}, {state}, {country}";
+		}
+}
+
+public class Customer
+{
+		private string name;
+		private Address address;
+
+		public Customer(string name, Address address)
+		{
+				this.name = name;
+				this.address = address;
+		}
+
+		public string Name => name;
+		public Address Address => address;
+
+		public bool IsInUSA()
+		{
+				return address.IsInUSA();
+		}
+}
+
+public class Product
+{
+		private string name;
+		private string productID;
+		private float price;
+		private int quantity;
+
+		public Product(string name, string productID, float price, int quantity)
+		{
+				this.name = name;
+				this.productID = productID;
+				this.price = price;
+				this.quantity = quantity;
+		}
+
+		public string Name => name;
+		public string ProductID => productID;
+		public float Price => price;
+		public int Quantity => quantity;
+
+		public float GetTotalCost()
+		{
+				return price * quantity;
+		}
+}
+
+public class Order
+{
+		private List<Product> products;
+		private Customer customer;
+
+		public Order(Customer customer)
+		{
+				this.customer = customer;
+				this.products = new List<Product>();
+		}
+
+		public void AddProduct(Product product)
+		{
+				products.Add(product);
+		}
+
+		public float CalculateTotalCost()
+		{
+				float totalCost = 0;
+				foreach (var product in products)
+				{
+						totalCost += product.GetTotalCost();
+				}
+
+				return totalCost;
+		}
+
+		public string GetPackingLabel()
+		{
+				StringBuilder packingLabel = new StringBuilder();
+				packingLabel.AppendLine("Packing Label:");
+				foreach (var product in products)
+				{
+						packingLabel.AppendLine($"{product.Quantity} x {product.Name}");
+				}
+				return packingLabel.ToString();
+		}
+
+		public string GetShippingLabel()
+		{
+				return $"Shipping Label:\n{customer.Name}\n{customer.Address}";
+		}
+}
+
 class Program
 {
 		static void Main(string[] args)
